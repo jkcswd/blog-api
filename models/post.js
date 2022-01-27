e34e77f63b94267cba9275707ed006da2cf912ea
James Spencer <jameskcs1254@gmail.com>
2022-01-27 12:32:23 +0000

    Create schema and model

--- models/post.js
+++ models/post.js
@@ -1,8 +1,17 @@
+const mongoose = require('mongoose');
+
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
-  a_string: String,
-  a_date: Date
+  title: {type: String, required: true, maxLength: 300},
+  text: {type: String, required: true},
+  user:{type: Schema.Types.ObjectId, ref: 'User', required: true},
+  datePublished: {type: Date ,required: true},
+  isPublished: {type: Boolean, required: true}
+});
+
+PostSchema.virtual('url').get(function() {
+  return '/api/post/' + this._id;
 });
 
 const Post = mongoose.model('Post', PostSchema );
--- models/user.js
+++ models/user.js
@@ -1,8 +1,18 @@
+const mongoose = require('mongoose');
+
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  a_string: String,
-  a_date: Date
+  firstName: {type: String, required: true},
+  lastName: {type: String, required: true},
+  username: {type: String, required: true},
+  password: {type: String, required: true},
+  email: {type: String, required: true},
+  isAdmin: {type: Boolean, required: true}
+});
+
+UserSchema.virtual('fullName').get(function(){
+  return this.firstName + this.lastName;
 });
 
 const User = mongoose.model('User', UserSchema );
