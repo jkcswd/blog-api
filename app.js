5f81a398f057dab05eac9c05233fba4c0e6ac040
James Spencer <jameskcs1254@gmail.com>
2022-01-27 12:36:48 +0000

    remove index route

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
 const userRouter = require('./routes/user');
 const postRouter = require('./routes/user');
 const commentRouter = require('./routes/user');
@@ -27,7 +26,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api', indexRouter);
 app.use('/api/user', userRouter);
 app.use('/api/post', postRouter);
 app.use('/api/comment', commentRouter);
