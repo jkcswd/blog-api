f1345b3334a804dfef8823a37d533970b3334188
James Spencer <jameskcs1254@gmail.com>
2022-01-28 12:07:10 +0000

    Create routes

--- routes/comment.js
+++ routes/comment.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const commentController = require('../controllers/commentController');
 
-router.get('/', (req, res, next) => {
-  
-});
+router.get('/', commentController.allCommentsGet );
+router.post('/', commentController.createNewComment);
+router.get('/:id', commentController.oneCommentGet );
+router.patch('/:id', commentController.updateComment);
+router.delete('/:id', commentController.deleteComment);
 
 module.exports = router;
