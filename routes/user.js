74bf6e243f04a09a3f82459b9e815f149bd8d5c1
James Spencer <jameskcs1254@gmail.com>
2022-01-28 11:21:14 +0000

    Create routes, pass in controllers

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const userController = require('../controllers/userController');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  
-});
+router.get('/', userController.allUsersGet );
+router.post('/', userController.createNewUser);
+router.get('/:id', userController.oneUserGet );
+router.patch('/:id', userController.updateUser);
+router.delete('/:id', userController.deleteUser);
+
 
 module.exports = router;
