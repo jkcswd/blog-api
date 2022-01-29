2779c2be253c5bba5997349b92d63cacc0b61e79
James Spencer <jameskcs1254@gmail.com>
2022-01-29 17:41:44 +0000

    Add JWT strategy

--- passport.js
+++ passport.js
@@ -2,6 +2,11 @@ const passport = require('passport');
 const bcrypt = require('bcryptjs');
 const LocalStrategy = require('passport-local').Strategy;
 const User = require('./models/user');
+const passportJWT = require("passport-jwt");
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt
+const dotenv = require('dotenv');
+dotenv.config();
 
 passport.use(new LocalStrategy((username, password, done) => {
   User.findOne({ username }, (err, user) => {
@@ -21,5 +26,21 @@ passport.use(new LocalStrategy((username, password, done) => {
         return done(null, false, { message: "Incorrect password" })
       }
     });
+  });
+}));
+
+passport.use(new JWTStrategy({
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey : process.env.JWT
+}, (jwtPayload, done) => {
+  User.findOne({id: jwtPayload.sub}, (err, user) => {
+      if (err) {
+          return done(err, false);
+      }
+      if (user) {
+          return done(null, user);
+      } else {
+          return done(null, false);
+      }
   });
 }));
