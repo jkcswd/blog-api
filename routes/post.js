ed55a374536a29fc2567e5d24140445ae8bfca1b
James Spencer <jameskcs1254@gmail.com>
2022-01-30 10:59:20 +0000

    Add auth on update,delete,create routes

--- routes/post.js
+++ routes/post.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 const postController = require('../controllers/postController');
 
+require('../passport');
+
 router.get('/', postController.allPostsGet );
-router.post('/', postController.createNewPost);
+router.post('/', passport.authenticate('jwt', {session: false}), postController.createNewPost);
 router.get('/:id', postController.onePostGet );
-router.patch('/:id', postController.updatePost);
-router.delete('/:id', postController.deletePost);
+router.patch('/:id', passport.authenticate('jwt', {session: false}), postController.updatePost);
+router.delete('/:id', passport.authenticate('jwt', {session: false}), postController.deletePost);
 
 module.exports = router;
