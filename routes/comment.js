b744d240837fa6f986e82109f38691009eef55db
James Spencer <jameskcs1254@gmail.com>
2022-02-01 12:33:41 +0000

    Add auth to routes

--- routes/comment.js
+++ routes/comment.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const commentController = require('../controllers/commentController');
+const passport = require('passport');
+
+require('../passport');
 
 router.get('/', commentController.allCommentsGet );
 router.post('/', commentController.createNewComment);
 router.get('/:id', commentController.oneCommentGet );
-router.patch('/:id', commentController.updateComment);
-router.delete('/:id', commentController.deleteComment);
+router.patch('/:id', passport.authenticate('jwt', {session: false}), commentController.updateComment);
+router.delete('/:id', passport.authenticate('jwt', {session: false}), commentController.deleteComment);
 
 module.exports = router;
